84146e9f503f8f9fa7c848301581d0b871c0d906
AseemBaranwal <aseembaranwal23@gmail.com>
2023-08-06 19:48:27 -0400

    Added solution for easy problem. Don't jinx it

--- 1838.frequency-of-the-most-frequent-element.cpp
+++ 1838.frequency-of-the-most-frequent-element.cpp
@@ -11,7 +11,22 @@ using namespace std;
 class Solution {
    public:
     int maxFrequency(vector<int>& nums, int k) {
-        
+        // Keep in mind that if we keep track of an average from left to right,
+        // we just need to check if the average is >= maxElement. Since we don't
+        // want a subarray, we can sort thr elements and the max element will be
+        // the at the right in window
+        sort(nums.begin(), nums.end());
+        long long subSum = 0;
+        int res = 0, l = 0;
+        for (int r = 0; r < nums.size(); r++) {
+            subSum += nums[r];
+            while ((long long)(subSum + k) / (r - l + 1) < nums[r]) {
+                subSum -= nums[l];
+                l++;
+            }
+            res = max(res, r - l + 1);
+        }
+        return res;
     }
 };
 // @lc code=end
