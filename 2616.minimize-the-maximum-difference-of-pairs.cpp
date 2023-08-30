a89138ecef89145a194a660dd830edc0bf2fe1ec
Aseem Baranwal <aseembaranwal23@gmail.com>
2023-08-30 19:49:36 -0400

    Added Solutions for Binary Search Problems

---
+++ 2616.minimize-the-maximum-difference-of-pairs.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=2616 lang=cpp
+ *
+ * [2616] Minimize the Maximum Difference of Pairs
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+   public:
+    int minimizeMax(vector<int>& nums, int p) {}
+};
+// @lc code=end
+
+/*
+[10,1,2,7,1,3]
+[1,1,2,3,7,10]
+*/
